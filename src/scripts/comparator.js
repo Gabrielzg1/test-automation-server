fb3fcfff6fe3d60aa778b709727c9545a9859f0b
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-01-17 10:49:04 -0300

    .

--- src/scripts/comparator.js
+++ src/scripts/comparator.js
@@ -1,25 +1,30 @@
 const fs = require("fs");
 
 module.exports = (number, subject, task, id) => {
-  const fileBase = fs.readFileSync(
-    `./src/subjects/${subject}/${task}/out/base/output_base${number}.txt`
-  );
-  const fileUpload = fs.readFileSync(
-    `./src/subjects/${subject}/${task}/out/upload/${id}/output_upload${number}.txt`
-  );
+	const fileBase = fs.readFileSync(
+		`./src/subjects/${subject}/${task}/out/base/output_base${number}.txt`
+	);
+	const fileUpload = fs.readFileSync(
+		`./src/subjects/${subject}/${task}/out/upload/${id}/output_upload${number}.txt`
+	);
 
-  if (
-    fileBase.toJSON().data.length != 0 &&
-    fileUpload.toJSON().data.length != 0
-  ) {
-    if (Buffer.compare(fileBase, fileUpload) === 0) {
-      return 1;
-    } else {
-      return 0;
-    }
-  } else {
-    if (fileBase.toJSON().data.length == 0)
-      console.log("Erro nas saídas esperadas");
-    else console.log("O arquivo enviado não contém saídas");
-  }
+	if (
+		fileBase.toJSON().data.length != 0 &&
+		fileUpload.toJSON().data.length != 0
+	) {
+		console.log(fileUpload.toJSON().data.length);
+		if (fileUpload.toJSON().data.length == 0) {
+			return 2;
+		}
+
+		if (Buffer.compare(fileBase, fileUpload) === 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	} else {
+		if (fileBase.toJSON().data.length == 0)
+			console.log("Erro nas saídas esperadas");
+		else console.log("O arquivo enviado não contém saídas");
+	}
 };

fbba51cb416027a40b6d1071b706d70724413cb7
Gabrielzg1 <gabrielzg173@gmail.com>
2023-01-23 00:18:15 -0300

    .

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ class App {
   }
   middlewares() {
     this.server.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header("Access-Control-Allow-Origin", "*");
       next();
     });
     this.server.use(express.json());

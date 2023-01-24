000ade880b99cd5a6f4bacac1ca9d115397589f5
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-01-23 21:46:33 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
-import express from "express";
+const express = require("express");
 import cors from "cors";
 import routes from "./routes";
 import "./database";
 
 class App {
-  constructor() {
-    this.server = express();
-    this.middlewares();
-    this.routes();
-    console.log("Running on http://localhost:2020");
-  }
-  middlewares() {
-    this.server.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      next();
-    });
-    this.server.use(express.json());
-    this.server.use(cors());
-  }
-  routes() {
-
-    this.server.use(routes);
-  }
+	constructor() {
+		this.server = express();
+		this.middlewares();
+		this.routes();
+		console.log("Running on http://localhost:2020");
+	}
+	middlewares() {
+		this.server.use(function (req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			next();
+		});
+		this.server.use(express.json());
+		this.server.use(cors());
+	}
+	routes() {
+		this.server.use(routes);
+	}
 }
 
 export default new App().server;

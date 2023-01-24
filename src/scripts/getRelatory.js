b32badee89b903fec49dcd73b1d62830e866fbc3
Gabrielzg1 <gabrielzg173@gmail.com>
2023-01-24 00:21:36 -0300

    ,

--- src/scripts/getRelatory.js
+++ src/scripts/getRelatory.js
@@ -1,12 +1,14 @@
 //import comparator from "./comparator";
 const comparator = require("./comparator")
 
-const getRelatory = async (subject, task, id) => {
+module.exports = async (subject, task, id) => {
 	var relatory = new Array();
 	for (let i = 1; i < 11; i++) {
 		var result = comparator(i, subject, task, id);
 		relatory.push(result);
 	}
 	return relatory;
-};
-module.exports = getRelatory
+}
+
+
+
